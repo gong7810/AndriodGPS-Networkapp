881d108de7c79370fce6c055fe918b4f7c0e9971
gong7810 <gong5518@naver.com>
2022-04-25 18:38:33 +0900

    드론 반납이벤트추가

--- app/src/main/java/com/example/mylocationdata/MainActivity.kt
+++ app/src/main/java/com/example/mylocationdata/MainActivity.kt
@@ -14,21 +14,72 @@ import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
 import android.widget.Button
-import java.io.DataInputStream
-import java.io.DataOutputStream
+import java.io.*
 import java.net.Socket
+import java.nio.ByteBuffer
 
+open class NetworkThread : Thread() {
+    private var mLocation: Location? = null
+    private var mIsLocationUnsynchronized: Boolean = false
+
+    open fun requestToSynchronize(location: Location) {
+        mLocation = location
+        mIsLocationUnsynchronized = true
+        Log.d("1", "1번째")
+    }
+
+    override fun run() {
+        lateinit var socket: Socket
+        lateinit var socketOutputStream: DataOutputStream
+        Log.d("2", "2번째")
+
+        try {
+            val ip = "203.255.57.129"
+            val port = 9999
+
+            socket = Socket(ip, port)
+            socketOutputStream = DataOutputStream(socket.getOutputStream())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        while (true) {
+            Log.d("3", "3번째")
+            if (mIsLocationUnsynchronized) {
+                Log.d("4", "4번째")
+                val buffer = ByteBuffer.allocate(24)
+                buffer.putDouble(mLocation!!.latitude)
+                buffer.putDouble(mLocation!!.longitude)
+                buffer.putDouble(mLocation!!.altitude)
+
+                try {
+                    socketOutputStream.write(buffer.array())
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+
+            mLocation = null
+            mIsLocationUnsynchronized = false
+            Log.d("5", "5번째")
+            sleep(10000) //샘플링 주기 = 서버에서 수신받는 주기
+
+        }
+    }
+}
 class MainActivity : AppCompatActivity() {
-    
-    private var mFusedLocationProviderClient : FusedLocationProviderClient? = null //현재위치를 가져오기 위한 변수
-    lateinit var mLastLocation : Location // 위치값을 가지고 있는 객체
-    internal lateinit var mLocationRequest : LocationRequest // 위치 정보 요청의 매개변수를 저장하는 변수
-    private  val REQUEST_PERMISSION_LOCATION = 10
 
-    lateinit var button : Button
-    lateinit var  text1 : TextView
-    lateinit var  text2 : TextView
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? =
+        null //현재위치를 가져오기 위한 변수
+    private lateinit var mLastLocation: Location // 위치값을 가지고 있는 객체
+    private lateinit var mLocationRequest: LocationRequest // 위치 정보 요청의 매개변수를 저장하는 변수
+    private var mNetworkThread: NetworkThread? = null// 소켓통신 스레드 객체
+    private val RequestPermissionLocation: Int = 10 //권한설정을 확인하기 위해 임의로 설정한 상수
 
+    private lateinit var button: Button
+    private lateinit var text1: TextView
+    private lateinit var text2: TextView
+    private lateinit var text3: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,40 +88,49 @@ class MainActivity : AppCompatActivity() {
         button = findViewById(R.id.button)
         text1 = findViewById(R.id.text1)
         text2 = findViewById(R.id.text2)
+        text3 = findViewById(R.id.text3)
 
-        mLocationRequest = LocationRequest.create().apply{
-
+        mLocationRequest = LocationRequest.create().apply {
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 10000 //화면에 표시되는 위치 갱신주기 단위는 ms
         }
 
         //버튼 이벤트를 통해 현재 위치
-        button.setOnClickListener{
-            if(checkPermissionForLocation(this)){
+        button.setOnClickListener {
+            if (checkPermissionForLocation(this)) {
                 startLocationUpdates()
             }
         }
     }
 
-    
-
     private fun startLocationUpdates() {
 
         //FusedLocationProviderClient의 인스턴스를 생성.
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             return
         }
         // 기기의 위치에 관한 정기 업데이트를 요청하는 메서드 실행
         // 지정한 루퍼 스레드(Looper.myLooper())에서 콜백(mLocationCallback)으로 위치 업데이트를 요청
-        mFusedLocationProviderClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper()!!)
+        mFusedLocationProviderClient!!.requestLocationUpdates(
+            mLocationRequest,
+            mLocationCallback,
+            Looper.myLooper()!!
+        )
     }
 
     // 시스템으로 부터 위치 정보를 콜백으로 받음
     private val mLocationCallback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
             // 시스템에서 받은 location 정보를 onLocationChanged()에 전달
-            locationResult.lastLocation
             onLocationChanged(locationResult.lastLocation)
         }
     }
@@ -80,6 +140,14 @@ class MainActivity : AppCompatActivity() {
         mLastLocation = location
         text1.text = "위도 : " + mLastLocation.latitude // 갱신 된 위도
         text2.text = "경도 : " + mLastLocation.longitude // 갱신 된 경도
+        text3.text = "고도 : " + mLastLocation.altitude // 갱신 된 고도
+
+        if (mNetworkThread == null) { //초기화 되있기때문에 if문 통과
+            mNetworkThread = NetworkThread()
+            mNetworkThread!!.start()
+        }
+
+        mNetworkThread!!.requestToSynchronize(mLastLocation) //스레드 주기 5,097ms
     }
 
 
@@ -91,7 +159,11 @@ class MainActivity : AppCompatActivity() {
                 true
             } else {
                 // 권한이 없으므로 권한 요청 알림 보내기
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSION_LOCATION)
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    RequestPermissionLocation
+                )
                 false
             }
         } else {
@@ -100,18 +172,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     // 사용자에게 권한 요청 후 결과에 대한 처리 로직
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        //if (requestCode == REQUEST_PERMISSION_LOCATION)
-        when(requestCode) {
-            REQUEST_PERMISSION_LOCATION -> {
-            if (grantResults.isNotEmpty() &&grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (requestCode == RequestPermissionLocation) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 startLocationUpdates()
 
             } else {
                 Log.d("ttt", "onRequestPermissionsResult() _ 권한 허용 거부")
                 Toast.makeText(this, "권한이 없어 해당 기능을 실행할 수 없습니다.", Toast.LENGTH_SHORT).show()
-            }
             }
         }
     }
